de138bc7a10f0bf8dd667ef5a97dd49113fd5526
FranDiana <din3711din@gmail.com>
2022-10-10 17:02:03 +0300

    xfhx

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<Integer> first  = new ArrayList<Integer>();
+        ArrayList<Integer> second = new ArrayList<Integer>();
+
+        first.add(36);
+        first.add(30);
+        first.add(-12);
+        first.add(42);
+        first.add(92);
+
+
+        second.add(-12);
+        second.add(89);
+        second.add(52);
+        second.add(42);
+        second.add(92);
+
+        second.retainAll(first);
+        System.out.println(second);
     }
+
+
 }
